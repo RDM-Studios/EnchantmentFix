7530c0b9e43f33a82321ebf150d9e27603332dba
RaveTr <yuyuss210@gmail.com>
2023-04-22 23:19:28 +0200

    Initial Commit

---
+++ src/main/java/com/rdm/enchantmentfix/EnchantmentFix.java
@@ -0,0 +1,15 @@
+package com.rdm.enchantmentfix;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import net.fabricmc.api.ModInitializer;
+
+public class EnchantmentFix implements ModInitializer {
+    public static final Logger LOGGER = LoggerFactory.getLogger("enchantmentfix");
+
+    @Override
+    public void onInitialize() {
+    	
+    }
+}
---
+++ src/main/java/com/rdm/enchantmentfix/EnchantmentFixDataGenerator.java
@@ -0,0 +1,12 @@
+package com.rdm.enchantmentfix;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class EnchantmentFixDataGenerator implements DataGeneratorEntrypoint {
+	
+    @Override
+    public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+    }
+}
